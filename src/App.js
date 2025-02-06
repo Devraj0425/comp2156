9893b9164aa06d86d6d59097f6db09f831c8853a
unknown <Devraj>
2025-02-06 18:32:33 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Studen ID: 101494054</h1>
+      <h1>Studen Name: Devraj Neupane</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developerd By: Devraj Neupane
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
